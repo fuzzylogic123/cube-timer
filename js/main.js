efac117faf594ea4f82a4f79bee154bee1a95cba
thecarefulcactus <89644288+thecarefulcactus@users.noreply.github.com>
2021-09-29 19:20:10 +1000

    added colours and stop/start function

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ let increment = 1;
 let offset;
 //retrives clock div
 let clock = document.getElementById('clock');
+let scramble = document.getElementById('scramble');
+let averages = document.getElementById('averages');
+let menus = document.getElementById('tool-bar');
 
 //starts the timer and sets callback function to update the timer ever increment
 function startTimer() {
@@ -38,55 +41,63 @@ function timeElapsed() {
 //global timeout handler
 let timerDelay;
 let spaceDownThreshold = 400;
+let startColor = '#32CD30';
+let holdColor = '#FF0000';
+let defaultColor = '#212529';
+const once = {
+    once: true
+};
 
-//waits for a key down, then sets a delay to run initiateTiming after delay
+//waits for a key down, then sets a delay to run timerReady after delay
 addEventListener("keydown", setDelay);
 
-//If the user releases the space bar before the timing threshold, then initiateTiming will not run
+//If the user releases the space bar before the timing threshold, then timerReady will not run
 addEventListener("keyup", clearWait);
 
-//checks if space bar is released and then starts timer
-function initiateTiming() {
-    clock.style.color = 'Green';
-    addEventListener("keyup", confirmStart);
+//runs if user has held space bar for required spaceDownThreshold
+function timerReady() {
+    clock.style.color = startColor;
+    scramble.style.display = 'none';
+    averages.style.display = 'none';
+    menus.style.display = 'none';
+    removeEventListener("keyup", clearWait);
+    removeEventListener("keydown", setDelay);
+    addEventListener("keyup", confirmStart, once);
 }
 
+//waits for user to release the space bar, and then starts the timer
 function confirmStart(e) {
     if (e.key === " ") {
-        clock.style.color = 'Black';
+        clock.style.color = defaultColor;
         startTimer();
-        removeEventListener("keydown", setDelay);
-        removeEventListener("keyup", confirmStart);
-        addEventListener("keydown", stopTimer);
+        addEventListener("keydown", stopTimer, once);
     }
 }
 
+//once the space key is held down, this turns the clock red until spaceDownThreshold has passed
 function setDelay(e) {
     if (e.key === " ") {
-        clock.style.color = 'Red';
-        removeEventListener("keydown", setDelay);
-        timerDelay = setTimeout(initiateTiming, spaceDownThreshold);
+        clock.style.color = holdColor;
+        timerDelay = setTimeout(timerReady, spaceDownThreshold);
     }
 }
 
+//if space is released before required time interval, then the timer start action is cancelled
 function clearWait(e) {
     if (e.key === " ") {
         clearTimeout(timerDelay);
-        clock.style.color = 'Black'
+        clock.style.color = defaultColor;
     }
 }
 
+//runs when space is clicked to stop timer
 function stopTimer(e) {
     if (e.key ===" ") {
         clearInterval(timerHandle);
-        removeEventListener("keydown", stopTimer);
-        addEventListener('keyup', restartTimer);
-    }
-}
-
-function restartTimer(e) {
-    if (e.key === " ") {
+        scramble.style.display = 'block';
+        averages.style.display = 'block';
+        menus.style.display = 'block';
         addEventListener("keydown", setDelay);
         addEventListener("keyup", clearWait);
-    } 
+    }
 }
