c85d0c40e7e77db1db82bc0d9e5bd6299477fdbc
thecarefulcactus <89644288+thecarefulcactus@users.noreply.github.com>
2021-09-29 16:50:11 +1000

    Implemented timer start functionality

--- js/main.js
+++ js/main.js
@@ -20,8 +20,9 @@ function startTimer() {
 //updates the timer
 function updateTimer() {
     totalMiliSec += timeElapsed();
-    let dispSeconds = Math.floor(totalMiliSec/1000);
+    let dispSeconds = Math.floor(totalMiliSec / 1000);
     let dispMiliSec = totalMiliSec % 1000;
+    dispMiliSec = `00${dispMiliSec}`.substr(-3);
     clock.innerHTML = `${dispSeconds}.${dispMiliSec}`;
 }
 
@@ -33,5 +34,29 @@ function timeElapsed() {
     return timeDiff
 }
 
+//global timeout handler
+let timerDelay;
+let spaceDownThreshold = 333;
 
-startTimer()
+//checks if space bar is released and then starts timer
+function initiateTiming() {
+    addEventListener("keyup", (e) => {
+        if (e.key === " ") {
+            startTimer();
+        }
+    })
+}
+
+//waits for a key down, then sets a delay to run initiateTiming after delay
+addEventListener("keydown", (e) => {
+    if (e.key === " ") {
+        timerDelay = setTimeout(initiateTiming, spaceDownThreshold);
+    }
+})
+
+//If the user releases the space bar before the timing threshold, then initiateTiming will not run
+addEventListener("keyup", (e) => {
+    if (e.key === " ") {
+        clearTimeout(timerDelay);
+    }
+})
