560590fff922da2c0260ff861835d391f261a0dc
Patrick Edwards <pedw0004@student.monash.edu>
2021-10-01 14:49:31 +1000

    add browser storage functions

--- js/shared.js
+++ js/shared.js
@@ -1,5 +1,44 @@
 "use strict";
 
+/**
+ * checkLSData function
+ * Used to check if any data in LS exists at a specific key
+ * @param {string} key LS Key to be used
+ * @returns true or false representing if data exists at key in LS
+ */
+ function checkLSData(key) {
+    if (localStorage.getItem(key) != null) {
+        return true;
+    }
+    return false;
+}
+/**
+ * retrieveLSData function
+ * Used to retrieve data from LS at a specific key. 
+ * @param {string} key LS Key to be used
+ * @returns data from LS in JS format
+ */
+function retrieveLSData(key) {
+    let data = localStorage.getItem(key);
+    try {
+        data = JSON.parse(data);
+    }
+    catch (err) { }
+    finally {
+        return data;
+    }
+}
+/**
+ * updateLSData function
+ * Used to store JS data in LS at a specific key
+ * @param {string} key LS key to be used
+ * @param {any} data data to be stored
+ */
+function updateLSData(key, data) {
+    let json = JSON.stringify(data);
+    localStorage.setItem(key, json);
+}
+
 class Solve {
     constructor(solveTime) {
         this._date = new Date;
