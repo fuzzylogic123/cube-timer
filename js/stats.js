b1dda16f1f3d4348c174a8e00f36a6f4291a0ad8
thecarefulcactus <89644288+thecarefulcactus@users.noreply.github.com>
2021-10-06 19:28:37 +1100

    fixed bug

--- js/stats.js
+++ js/stats.js
@@ -4,7 +4,7 @@ function solveListHTML(session) {
     const list = document.getElementById('solve-list')
     for (let i = 0; i < session.solveList.length; i++) {
         const solve = session.solveList[i];
-        document.getElementById('table-contents').innerHTML = `
+        document.getElementById('table-contents').innerHTML += `
         <tr>
         <td>${formatTime(solve.time)}</td>
         <td>${solve.scramble}</td>
