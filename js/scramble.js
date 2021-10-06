3017ced1f01655561759a80ccb5e143e1fdc6796
Patrick Edwards <pedw0004@student.monash.edu>
2021-10-06 19:51:07 +1100

    renaming files

---
+++ js/scramble.js
@@ -0,0 +1,44 @@
+"use strict"
+
+//generates scramble
+let scrambleNotation = [["R", "R'", "R2"], ["L", "L'", "L2"], ["F", "F'", "F2"], ["B", "B'", "B2"], ["U", "U'", "U2"], ["D", "D'", "D2"]]
+
+let currentScramble = scrambleGen(scrambleNotation)
+scrambleToHTML(currentScramble);
+
+function selectRandom(array) {
+    return Math.floor(Math.random() * array.length);
+}
+
+//function to generate a new scramble
+function scrambleGen(scrambleNotation, len = 20) {
+    let scramble = '';
+    let removed;
+    for (let i = 0; i < len; i++) {
+        let groupIndex = selectRandom(scrambleNotation);
+        let scrambleGroup = scrambleNotation[groupIndex];
+        let letter = scrambleGroup[selectRandom(scrambleGroup)];
+        scramble += letter + ' ';
+        //replace the previously removed entry if it exists
+        if (i > 0) scrambleNotation.push(removed[0]);
+        removed = scrambleNotation.splice(groupIndex, 1);
+    }
+    scrambleNotation.push(removed[0]);
+    return scramble.substring(0, scramble.length - 1);
+}
+
+//converts scramble array to HTML
+function scrambleToHTML(scramble) {
+    scramble = scramble.split(' ');
+    console.log(scramble);
+    const scrambleRef = document.getElementById('scramble');
+    scrambleRef.innerHTML = '';
+    let h2 = document.createElement("h2");
+    scrambleRef.appendChild(h2);
+    for (let i = 0; i < scramble.length; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add('scrambleLetter');
+        newDiv.innerHTML = scramble[i];
+        h2.appendChild(newDiv);
+    }
+}
--- js/timer.js
+++ js/timer.js
@@ -16,7 +16,6 @@ let holdColor = '#FF0000';
 let defaultColor = 'white';
 //retrives DOM elements
 const clock = document.getElementById('clock');
-const scramble = document.getElementById('scramble');
 const ao5 = document.getElementById('ao5');
 const ao12 = document.getElementById('ao12');
 const menus = document.getElementById('tool-bar');
@@ -31,48 +30,7 @@ statIcon.addEventListener('click', () => {
     window.location.href = "./stats.html";
 })
 
-//generates scramble
-let scrambleNotation = [["R", "R'", "R2"], ["L", "L'", "L2"], ["F", "F'", "F2"], ["B", "B'", "B2"], ["U", "U'", "U2"], ["D", "D'", "D2"]]
 
-let currentScramble = scrambleGen(scrambleNotation)
-scrambleToHTML(currentScramble);
-
-function selectRandom(array) {
-    return Math.floor(Math.random() * array.length);
-}
-
-//function to generate a new scramble
-function scrambleGen(scrambleNotation, len = 20) {
-    let scramble = '';
-    let removed;
-    for (let i = 0; i < len; i++) {
-        let groupIndex = selectRandom(scrambleNotation);
-        let scrambleGroup = scrambleNotation[groupIndex];
-        let letter = scrambleGroup[selectRandom(scrambleGroup)];
-        scramble += letter + ' ';
-        //replace the previously removed entry if it exists
-        if (i > 0) scrambleNotation.push(removed[0]);
-        removed = scrambleNotation.splice(groupIndex, 1);
-    }
-    scrambleNotation.push(removed[0]);
-    return scramble;
-}
-
-//converts scramble array to HTML
-function scrambleToHTML(scramble) {
-    scramble = scramble.split(' ');
-    console.log(scramble);
-    let scrambleRef = document.getElementById('scramble');
-    scrambleRef.innerHTML = '';
-    let h2 = document.createElement("h2");
-    scrambleRef.appendChild(h2);
-    for (let i = 0; i < scramble.length; i++) {
-        const newDiv = document.createElement("div");
-        newDiv.classList.add('scrambleLetter');
-        newDiv.innerHTML = scramble[i];
-        h2.appendChild(newDiv);
-    }
-}
 
 /*timer code*/
 
