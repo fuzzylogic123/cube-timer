2102d39677b22774001134b66dfec20b50824ea5
Patrick Edwards <pedw0004@student.monash.edu>
2021-10-07 20:27:57 +1100

    made menu transparent

--- js/stats.js
+++ js/stats.js
@@ -3,7 +3,7 @@
 function solveListHTML(session) {
   const tableContents = document.getElementById('table-contents');
   if (session.solveList.length === 0) {
-    tableContents.innerHTML = "<tr><td>-</td><td>-</td><td>-</td></tr>"
+    tableContents.innerHTML = "<tr><td>-</td><td>-</td><td>-</td><td> </td></tr>"
   }
     for (let i = 0; i < session.solveList.length; i++) {
         const solve = session.solveList[i];
