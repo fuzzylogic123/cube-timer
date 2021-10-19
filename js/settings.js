21809e9f50fdb4a74a8aafb5981b85d309f1ce10
thecarefulcactus <89644288+thecarefulcactus@users.noreply.github.com>
2021-10-19 22:53:11 +1100

    background implimentation

--- js/settings.js
+++ js/settings.js
@@ -19,5 +19,25 @@ submit.addEventListener('click', () => {
     settings.background = backgroundSelection.value;
     settings.manualEntry = manualEntry.checked;
     settings.scrambleLen = Number(scrambleLen.value);
+    setBackground();
     updateLSData(settingsKey, settings);
 });
+
+function setBackground() {
+    const backgroundIndex = settings.background;
+    const source = backgrounds[backgroundIndex];
+    console.log(source);
+    for (let i = 0; i < source.length; i++) {
+        const currentSource = source[i];
+        let myScript = document.createElement("script");
+        myScript.setAttribute("src", currentSource);
+        document.body.appendChild(myScript);
+    }
+}
+
+const backgrounds = {
+    0: ['https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js', './backgrounds/particles/particles.js'],
+    1: [''],
+    2: ['./backgrounds/balls/balls.js'],
+    // 3: ['./img/colored-smoke.jpg']
+};
