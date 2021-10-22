83121f2dae379fc86413d3295f4bce8fd4edfd89
Patrick Edwards <pedw0004@student.monash.edu>
2021-10-22 21:34:55 +1100

    slight adjustments

--- js/settings.js
+++ js/settings.js
@@ -32,8 +32,14 @@ settingsIcon.addEventListener('click', () => {
     manualEntry.checked = settings.manualEntry;
     const sessionType = sessionList.list[sessionList.active].solveType;
     scrambleLenRef.value = settings.scrambleLen[sessionType];
-    if (backgroundSelection.value > 1) backgroundPreview.src = backgrounds[backgroundSelection.value];
-})
+    console.log(backgroundSelection.value);
+    if (backgroundSelection.value > 2) {
+        backgroundPreview.src = backgrounds[backgroundSelection.value];
+        document.querySelector('#colorPicker').style.display = 'none';
+    } else {
+        backgroundPreview.style.display = 'none'
+    }
+});
 
 //when the user saves their changes
 saveChanges.addEventListener('click', () => {
@@ -72,12 +78,13 @@ initPreferences()
 const backgroundDropdown = document.querySelector('#backgroundSelection');
 
 backgroundDropdown.addEventListener('change', (e) => {
+    const backgroundPreview = document.querySelector('#backgroundPreview');
     //if the background is set to a picture, background color picker is invisible
     document.querySelector('#colorPicker').style.display = e.target.value > 1 ? 'none' : '';
     if (backgroundSelection.value > 2) {
         backgroundPreview.style.display = 'inline-block'
         backgroundPreview.src = backgrounds[backgroundSelection.value]
     } else {
-        backgroundPreview.style.display = 'none'
+        backgroundPreview.style.display = 'none';
     };
 })
