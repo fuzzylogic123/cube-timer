001584db559883426482adee8ac54b0db78f4051
Patrick Edwards <pedw0004@student.monash.edu>
2021-10-27 02:25:33 +1100

    updated graphs page

--- js/graphs.js
+++ js/graphs.js
@@ -1,5 +1,7 @@
 "use strict"
 
+console.log(solveTimes);
+
 let plotData = {
     labels: labels,
     datasets: [
--- js/stats.js
+++ js/stats.js
@@ -1,49 +1,110 @@
 "use strict"
 
+let labels;
+let solveTimes;
+let distributionLabels;
+let distribution;
+
+function initDropdownGraphs() {
+    const listOfSessions = sessionList.list;
+    activeIndex = sessionList.active;
+    const dropdown = document.querySelector('#dropdown-graphs');
+    dropdown.innerHTML = '';
+    updateLSData(sessionKey, sessionList);
+    for (let i = 0; i < listOfSessions.length; i++) {
+        const session = listOfSessions[i];
+        let selected = i === activeIndex;
+        const option = new Option(`${session.name} - ${session.solveType}`, i, false, selected);
+        dropdown.appendChild(option);
+    }
+    dropdown.addEventListener('change', (e) => {
+        activeIndex = e.target.value;
+        sessionList.active = activeIndex;
+        calcStats();
+        updateLSData(sessionKey, sessionList);
+    });
+}
 
 let session = sessionList.list[activeIndex];
 
-let solveTimes = [];
-let labels = Array.from({ length: session.solveList.length }, (_, i) => i + 1);
-for (let i = session.solveList.length - 1; i >= 0; i--) {
-    const solveTime = session.solveList[i].time / 1000;
-    solveTimes.push(solveTime);
-}
+initDropdownGraphs();
+calcStats();
 
-const average = (array) => array.reduce((a, b) => a + b) / array.length;
-const averageRef = document.querySelector('#average');
-
-if (solveTimes.length > 0) {
-    const averageTime = average(solveTimes);
-    averageRef.innerHTML = (averageTime).toFixed(2);
-} else {
-    averageRef.innerHTML = 'N/A'
-}
-
-const solveNumRef = document.querySelector('#numberOfSolves').innerHTML = session.solveList.length;
-
-//solve distibution
-const INCREMENT_WIDTH = 10;
-
-let min = Math.min(...solveTimes);
-let max = Math.max(...solveTimes);
-
-let distribution = [];
-let j = 0;
-
-let currentThreshold = Math.floor(min);
-let solveCount = 0;
-let distributionLabels = [];
-while (solveCount < solveTimes.length) {
-    distribution[j] = 0;
-    for (let i = 0; i < solveTimes.length; i++) {
-        const solveTime = solveTimes[i];
-        if (solveTime > currentThreshold && solveTime < currentThreshold + 1) {
-            distribution[j] += 1;
-            solveCount += 1;
+function calcStats() {
+    solveTimes = [];
+    for (let i = session.solveList.length - 1; i >= 0; i--) {
+        const solveTime = session.solveList[i].time / 1000;
+        if (!session.solveList[i].penalty) {
+            solveTimes.push(solveTime);
         }
     }
-    currentThreshold += 1;
-    distributionLabels.push(currentThreshold);
-    j++;
+    console.log(solveTimes);
+    labels = Array.from({ length: solveTimes.length }, (_, i) => i + 1);
+
+    const average = (array) => array.reduce((a, b) => a + b) / array.length;
+    const averageRef = document.querySelector('#average');
+
+    let averageTime;
+    if (solveTimes.length > 0) {
+        averageTime = average(solveTimes);
+        averageRef.innerHTML = (averageTime).toFixed(2);
+    } else {
+        averageRef.innerHTML = 'N/A'
+    }
+
+    const solveNumRef = document.querySelector('#numberOfSolves').innerHTML = session.solveList.length;
+
+    //solve distibution
+    const INCREMENT_WIDTH = 10;
+
+    let min = Math.min(...solveTimes);
+    let max = Math.max(...solveTimes);
+
+    distribution = [];
+    let j = 0;
+
+    let currentThreshold = Math.floor(min);
+    let solveCount = 0;
+    distributionLabels = [];
+    while (solveCount < solveTimes.length) {
+        distribution[j] = 0;
+        for (let i = 0; i < solveTimes.length; i++) {
+            const solveTime = solveTimes[i];
+            if (solveTime > currentThreshold && solveTime < currentThreshold + 1) {
+                distribution[j] += 1;
+                solveCount += 1;
+            }
+        }
+        currentThreshold += 1;
+        distributionLabels.push(`${currentThreshold - 1} - ${currentThreshold}`);
+        j++;
+    }
+
+    //normal  distribution
+
+    function ncdf(x, mean, std) {
+        var x = (x - mean) / std
+        var t = 1 / (1 + .2315419 * Math.abs(x))
+        var d = .3989423 * Math.exp(-x * x / 2)
+        var prob = d * t * (.3193815 + t * (-.3565638 + t * (1.781478 + t * (-1.821256 + t * 1.330274))))
+        if (x > 0) prob = 1 - prob
+        return prob
+    }
+
+    function getStandardDeviation(array) {
+        const n = array.length
+        const mean = array.reduce((a, b) => a + b) / n
+        return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+    }
+
+
+    const timeInput = document.querySelector('#userTime');
+    timeInput.addEventListener('change', () => {
+        const sd = getStandardDeviation(solveTimes);
+        const chanceRef = document.querySelector('#chance');
+        chanceRef.innerHTML = (ncdf(timeInput.value, averageTime, sd) * 100).toFixed(2) + '%';
+    });
+
 }
+
+console.log(solveTimes);
