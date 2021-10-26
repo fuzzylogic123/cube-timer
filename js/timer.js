beb4eae17545faeb7bf8767390e3f5cc855ec53c
Patrick Edwards <pedw0004@student.monash.edu>
2021-10-26 19:06:09 +1100

    small change

--- js/timer.js
+++ js/timer.js
@@ -34,6 +34,7 @@ plusTwo.style.display = "none";
 //fill averages on screen
 ao5.innerHTML = session.getAverage(5);
 ao12.innerHTML = session.getAverage(12);
+alert("Hugh's arogance is unparalleled");
 
 statIcon.addEventListener("click", () => {
   updateLSData(sessionKey, sessionList);
